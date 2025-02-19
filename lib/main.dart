4cf1f00fe064ecdff6ede58475eaf87d2bcd1e8c
Rodrigo <rodrigodil@live.com>
2025-02-19 16:33:36 -0300

    Add transação, rolagem, abertura do teclado automaticamente

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'models/transaction.dart';
+import 'components/transaction_user.dart';
 
 
 main() => runApp(const ExpensesApp());
@@ -13,24 +13,8 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
 
-  final titleController = TextEditingController(); 
-  final valueController = TextEditingController(); 
-
-  final _transactions = [
-    Transaction(
-      id: 't1',
-      title: 'Presente pra Lutcha',
-      value: 310.76,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Conta do Iate',
-      value: 211.32,
-      date: DateTime.now(),
-    ),
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -38,57 +22,21 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Despesas Pessoais'),
       ),
-      body: Column(        
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          SizedBox(
-            child: Card(
-              color: Colors.blue,
-              elevation: 5,
-              child: Text('Gráfico'),
-            ),
-          ),
-
-          
-          TransactionList(_transactions),
-          Card(
-            elevation: 5,
-            child: Padding(
-              padding: const EdgeInsets.all(10),
-              child: Column(
-                children: [
-                  TextField(
-                    controller: titleController,
-                    decoration: InputDecoration(
-                      labelText: 'Título',
-                    )
-                  ),
-                  TextField(
-                    controller: valueController,
-                    decoration: InputDecoration(
-                      labelText: 'Valor (R\$)',
-                    )
-                  ),
-                  // Botão de adicionar transação
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      TextButton(                      
-                        child: const Text(
-                          'Nova Transação',
-                          style: TextStyle(
-                            color: Colors.purple,
-                          ),
-                        ),
-                        onPressed: () {},
-                      )
-                    ],
-                  ),
-                ],
+      body: SingleChildScrollView(
+        child: Column(        
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SizedBox(
+              child: Card(
+                color: Colors.blue,
+                elevation: 5,
+                child: Text('Gráfico'),
               ),
             ),
-            )
-        ],
+          
+            TransactionUser(),
+          ],
+        ),
       ),
     );
   }
